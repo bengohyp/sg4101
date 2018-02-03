5c0c9d626dc7d4c480cf9216249f0cddcd9eed22
bengohyp <bengohyp@gmail.com>
2018-02-03 12:20:14 -0500

    Updated Workshop 2

---
+++ Workshop-2/compile.sh
@@ -0,0 +1,6 @@
+#navigate to source code directory
+cd /home/cabox/workspace/Workshop-2/src
+
+echo [Working] Compiling ClubApplication source code...
+javac -d ../bin sg/edu/nus/iss/club/ClubApplication.java
+echo [Done] ClubApplication compilation complete!
---
+++ Workshop-2/run.sh
@@ -0,0 +1,8 @@
+#compile source codes
+source /home/cabox/workspace/Workshop-2/compile.sh
+
+#navigate to working directory
+cd /home/cabox/workspace/Workshop-2/bin
+
+#run ClubApplication java program
+java sg.edu.nus.iss.club.ClubApplication
